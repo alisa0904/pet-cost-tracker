38c11672cc853477686aff0051d895e45cfaa749
Табельская Алиса <alya.tabelskaya@bk.ru>
2026-01-11 18:40:30 +0500

    Add redirect from root to pets home page

--- petcosttracker/urls.py
+++ petcosttracker/urls.py
@@ -1,4 +1,3 @@
-# petcosttracker/urls.py - ИЗМЕНИТЬ ТАК
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -7,11 +6,11 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Перенаправление с корня на главную страницу pets
+    
     path('', RedirectView.as_view(pattern_name='pets:home', permanent=False)),
     
-    # Все маршруты pets доступны от корня
-    path('', include('pets.urls')),  # <-- БЕЗ префикса 'pets/'
+    # Все маршруты приложения pets доступны по префиксу /pets/
+    path('pets/', include('pets.urls')),
     
     # Аутентификация
     path('accounts/login/', auth_views.LoginView.as_view(
